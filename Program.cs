4ea1c8f13f7d3ae4c81959c20da58e80888dcfed
A253562 <A253562@LSBSA-9JJCSV3>
2023-12-07 01:42:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Attribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Song holiday = new Song("Holiday", "Green Day", 200);
+            Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
+
+            DisplaySongInfo(holiday);
+            DisplaySongInfo(kashmir);
+
+            Console.ReadLine();
+        }
+
+        static void DisplaySongInfo(Song song)
+        {
+            Console.WriteLine($"Title: {song.Title}\nArtists: {song.Artists}\nDuration: {song.Duration} seconds\n");
+        }
+    }
+}
---
+++ Songs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Attribute
+{
+    internal class Song
+    {
+        public string Title { get; }
+        public string Artists { get; }
+        public int Duration { get; }
+
+        private static int songCount = 0;
+
+        public Song(string title, string artists, int duration)
+        {
+            Title = title;
+            Artists = artists;
+            Duration = duration;
+            songCount++;
+        }
+    }
+}
